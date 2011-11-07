d3c4b2e6446fd68a134eed035e4e18e64ff25e8b
Saz <me@saz.sh>
2011-11-07 19:18:52 +0100

    remove useless curly bracket

--- manifests/ssh_configline.pp
+++ manifests/ssh_configline.pp
@@ -1,9 +1,7 @@
 define ssh::ssh_configline ($value) {
-    augeas {
-        "ssh_config_$name":
+    augeas { "ssh_config_$name":
             context => "/files/etc/ssh/ssh_config",
             changes => "set $name $value",
             onlyif  => "get $name != $value",
-        }
     }
 }
