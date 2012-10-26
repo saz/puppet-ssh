83612c9f42f6887f49406a536d44fbff747b9ffb
Steffen Zieger <me@saz.sh>
2012-10-26 19:20:42 +0200

    use $::osfamily instead of $::operatingsystem

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ssh::params {
-  case $::operatingsystem {
-    /(Ubuntu|Debian)/: {
+  case $::osfamily {
+    debian: {
       $server_package_name = 'openssh-server'
       $client_package_name = 'openssh-client'
       $sshd_config = '/etc/ssh/sshd_config'
@@ -8,7 +8,7 @@ class ssh::params {
       $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
       $service_name = 'ssh'
     }
-    /(CentOS|Fedora|RedHat)/: {
+    redhat: {
       $server_package_name = 'openssh-server'
       $client_package_name = 'openssh-clients'
       $sshd_config = '/etc/ssh/sshd_config'
@@ -17,7 +17,11 @@ class ssh::params {
       $service_name = 'sshd'
     }
     default: {
-      fail("Unsupported platform: ${::operatingsystem}")
+      case $::operatingsystem {
+        default: {
+          fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+        }
+      }
     }
   }
 }
