d6571f8c43ac55d20a6afd8a8ce3f86ac4b0d7a4
Steffen Zieger <me@saz.sh>
2012-11-23 17:42:11 +0100

    use a variable for host_aliases

--- manifests/hostkeys.pp
+++ manifests/hostkeys.pp
@@ -1,11 +1,13 @@
 class ssh::hostkeys {
+  $host_aliases = [ $::fqdn, $::hostname, $::ipaddress ]
+
   @@sshkey { "${::fqdn}_dsa":
-    host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+    host_aliases => $host_aliases,
     type         => dsa,
     key          => $::sshdsakey,
   }
   @@sshkey { "${::fqdn}_rsa":
-    host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+    host_aliases => $host_aliases,
     type         => rsa,
     key          => $::sshrsakey,
   }
