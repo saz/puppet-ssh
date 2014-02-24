ef5666f3555d549dd4203c5381592979c9eddf7e
Steffen Zieger <me@saz.sh>
2014-02-24 22:55:08 +0100

    add disable_user_known_hosts variable for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
 class ssh (
+  $disable_user_known_hosts = true,
   $sshd_default_options = $ssh::params::sshd_default_options,
   $sshd_options         = {},
   $ssh_default_options  = $ssh::params::ssh_default_options,
