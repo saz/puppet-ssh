dbabc49c150abe8f532c2eebc865a3e131e81c85
Marco Wessel <marco@poop.nl>
2014-03-04 16:34:22 +0100

    fix syntax

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -1,7 +1,7 @@
-class ssh::client::install 
+class ssh::client::install {
   if !defined(Package[$ssh::params::client_package_name]) {
     package { $ssh::params::client_package_name:
-      ensure => $ensure,
+      ensure => $ssh::client::ensure,
     }
   }
 }
--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,10 +1,8 @@
-class ssh::server::install 
+class ssh::server::install {
   include ssh::params
   if !defined(Package[$ssh::params::server_package_name]) {
     package { $ssh::params::server_package_name:
-      ensure   => $ensure,
-      blahblah => $ensure,
-
+      ensure   => $ssh::server::ensure,
     }
   }
 }
