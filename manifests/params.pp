3af5933387f44a010892d40b7042cff7c7e321dd
Jason Antman <Jason.Antman@coxinc.com>
2014-03-20 15:46:42 -0400

    add support for Archlinux

--- manifests/params.pp
+++ manifests/params.pp
@@ -29,6 +29,16 @@ class ssh::params {
       $service_name = 'sshd'
       $sftp_server_path = '/usr/lib/openssh/sftp-server'
     }
+    Archlinux: {
+      $server_package_name = 'openssh'
+      $client_package_name = 'openssh'
+      $sshd_dir = '/etc/ssh'
+      $sshd_config = '/etc/ssh/sshd_config'
+      $ssh_config = '/etc/ssh/ssh_config'
+      $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
+      $service_name = 'sshd.service'
+      $sftp_server_path = '/usr/lib/ssh/sftp-server'
+    }
     default: {
       case $::operatingsystem {
         gentoo: {
