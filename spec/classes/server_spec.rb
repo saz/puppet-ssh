e1349a0534598dea89e6334bfa07ade8964094c6
Steffen Zieger <me@saz.sh>
2014-04-25 17:21:33 +0200

    add spec tests for ArchLinux (thanks jantman), fixes #37

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -72,6 +72,53 @@ describe 'ssh::server' do
               (content.split("\n") & expected_lines).should =~ expected_lines
             end
           end
+          describe "on Arch" do
+            let :facts do
+            {
+                :osfamily => 'Archlinux',
+                :lsbdistdescription => 'Arch Linux',
+                :lsbdistid => 'Arch',
+                :operatingsystem => 'Archlinux',
+                :interfaces => 'enp4s0',
+                :ipaddress_eth0 => '192.168.1.1'
+            }
+            end
+
+            it { should contain_class('ssh::params') }
+            it { should contain_package('openssh')
+                 .with(:ensure => param_hash[:ensure], :name => 'openssh')
+            }
+
+            it { should contain_file('/etc/ssh/sshd_config').with(
+              'owner' => 0,
+              'group' => 0
+            )}
+
+            it { should contain_service('sshd.service').with(
+              'ensure' => 'running',
+              'enable' => true,
+              'hasrestart' => true,
+              'hasstatus' => true
+            )}
+
+            it 'should compile the template based on the class parameters' do
+              content = param_value(
+                subject,
+                'file',
+                '/etc/ssh/sshd_config',
+                'content'
+              )
+              expected_lines = [
+                'ChallengeResponseAuthentication no',
+                'X11Forwarding yes',
+                'PrintMotd no',
+                'AcceptEnv LANG LC_*',
+                'Subsystem sftp /usr/lib/ssh/sftp-server',
+                'UsePAM yes'
+              ]
+              (content.split("\n") & expected_lines).should =~ expected_lines
+            end
+          end
         end
       end
     end
