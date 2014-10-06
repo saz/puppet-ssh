a07ef32736302a6fb90066f17bf4916de43b1a90
Cristian Falcas <falcas@optymyze.com>
2014-10-06 15:43:47 +0300

    rebase

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -78,11 +78,19 @@ describe 'ssh::server' do
             {
                 :osfamily           => 'Archlinux',
                 :lsbdistdescription => 'Arch Linux',
+<<<<<<< HEAD
                 :lsbdistid          => 'Arch',
                 :operatingsystem    => 'Archlinux',
                 :interfaces         => 'enp4s0',
                 :ipaddress_eth0     => '192.168.1.1',
                 :concat_basedir     => '/tmp',
+=======
+                :lsbdistid => 'Arch',
+                :operatingsystem => 'Archlinux',
+                :interfaces => 'enp4s0',
+                :ipaddress_eth0 => '192.168.1.1',
+                :concat_basedir => '/tmp'
+>>>>>>> fix service class, add facts for travis
             }
             end
 
