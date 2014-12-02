31464c2891caecfe72176e23692f383dd574663a
Chris Smith <chris.smith@widerplan.com>
2014-12-02 14:20:23 +0000

    Change to structured fact providing breakdown of OpenSSH version

--- lib/facter/ssh_client_version.rb
+++ lib/facter/ssh_client_version.rb
@@ -1,10 +1,17 @@
 Facter.add("ssh_client_version") do
   setcode do
-    Facter::Util::Resolution.exec('ssh -V 2>&1')
+    version = Facter::Util::Resolution.exec('ssh -V 2>&1')
       .lines
       .to_a
       .select { |line| line.match(/^OpenSSH_/) }
       .first
-      .gsub(/^OpenSSH_([^ ]+).*$/, '\1')
+      .rstrip
+
+    {
+      'major' => version.gsub(/^OpenSSH_([0-9]+\.[0-9]+).*$/, '\1'),
+      'release' => version.gsub(/^OpenSSH_([0-9]+\.[0-9]+(?:\.[0-9]+)?).*$/, '\1'),
+      'portable' => version.gsub(/^OpenSSH_[0-9]+\.[0-9]+(?:\.[0-9]+)?p([0-9]+).*$/, '\1'),
+      'full' => version.gsub(/^OpenSSH_([^ ]+).*$/, '\1'),
+    }
   end
 end
--- lib/facter/ssh_server_version.rb
+++ lib/facter/ssh_server_version.rb
@@ -3,11 +3,18 @@ Facter.add("ssh_server_version") do
     # sshd doesn't actually have a -V option (hopefully one will be added),
     # by happy coincidence the usage information that is printed includes the
     # version number.
-    Facter::Util::Resolution.exec('sshd -V 2>&1')
+    version = Facter::Util::Resolution.exec('sshd -V 2>&1')
       .lines
       .to_a
       .select { |line| line.match(/^OpenSSH_/) }
       .first
-      .gsub(/^OpenSSH_([^ ]+).*$/, '\1')
+      .rstrip
+
+    {
+      'major' => version.gsub(/^OpenSSH_([0-9]+\.[0-9]+).*$/, '\1'),
+      'release' => version.gsub(/^OpenSSH_([0-9]+\.[0-9]+(?:\.[0-9]+)?).*$/, '\1'),
+      'portable' => version.gsub(/^OpenSSH_[0-9]+\.[0-9]+(?:\.[0-9]+)?p([0-9]+).*$/, '\1'),
+      'full' => version.gsub(/^OpenSSH_([^ ]+).*$/, '\1'),
+    }
   end
 end
