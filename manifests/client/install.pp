f63ae3b0b15927625b06e7db093ac661743a9ecf
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2014-12-06 01:31:04 +0100

    Add explicit allow_virtual parameters
    As the default will change, a warning is issued if this is not explicitly stated
    with puppet 3.7.3.

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -2,7 +2,8 @@ class ssh::client::install {
   if $ssh::params::client_package_name {
     if !defined(Package[$ssh::params::client_package_name]) {
       package { $ssh::params::client_package_name:
-        ensure => $ssh::client::ensure,
+        ensure        => $ssh::client::ensure,
+        allow_virtual => false,
       }
     }
   }
