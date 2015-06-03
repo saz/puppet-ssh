5526b90bcbb1862acf86677ad214f98c7bd62d4f
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-06-03 17:26:05 -0500

    Fixes to make puppet-lint happy

--- manifests/server/match_block.pp
+++ manifests/server/match_block.pp
@@ -1,4 +1,4 @@
-define ssh::server::match_block ($type = 'user', $order = 50, $options,) {
+define ssh::server::match_block ($options, $type = 'user', $order = 50,) {
   concat::fragment { "match_block ${name}":
     target  => $ssh::params::sshd_config,
     content => template("${module_name}/sshd_match_block.erb"),
