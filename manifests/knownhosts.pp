0ce1973e06e1129fcf3862783835d679128c80a7
Tobias Brunner <tobias.brunner@vshn.ch>
2015-06-23 10:45:11 +0200

    do not force ssh hostkeys to be present

--- manifests/knownhosts.pp
+++ manifests/knownhosts.pp
@@ -1,5 +1,3 @@
 class ssh::knownhosts ($storeconfigs_group = undef) {
-  Sshkey <<| tag == "hostkey_${storeconfigs_group}" |>> {
-    ensure => present,
-  }
+  Sshkey <<| tag == "hostkey_${storeconfigs_group}" |>>
 }
