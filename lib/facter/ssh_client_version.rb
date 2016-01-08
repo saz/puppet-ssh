53db1fece7e0815d8b66e8b0efea49d1924a372a
Sergey Kulanov <skulanov@mirantis.com>
2016-01-08 10:47:12 +0200

    Use ssh command to get openssh client version

--- lib/facter/ssh_client_version.rb
+++ lib/facter/ssh_client_version.rb
@@ -1,7 +1,7 @@
 Facter.add("ssh_client_version_full") do
   confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
 
-  version = Facter::Util::Resolution.exec('sshd -V 2>&1').
+  version = Facter::Util::Resolution.exec('ssh -V 2>&1').
     lines.
     to_a.
     select { |line| line.match(/^OpenSSH_/) }.
