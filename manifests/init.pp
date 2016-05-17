saz/puppet-ssh

commit 8aa2b7bb987120c6b46238d158064fb83e4790ec

Fix future_parser bug with undef options converted to empty string
See
https://groups.google.com/forum/#!msg/puppet-users/Q0jrR9grbFI/oAc0Y5hTGtsJ
for more information about this bug

manifests/init.pp
@@ -18,16 +18,19 @@ class ssh (
 
   $fin_server_options = $hiera_server_options ? {
     undef   => $server_options,
+    ''      => $client_options,
     default => $hiera_server_options,
   }
 
   $fin_client_options = $hiera_client_options ? {
     undef   => $client_options,
+    ''      => $client_options,
     default => $hiera_client_options,
   }
 
   $fin_users_client_options = $hiera_users_client_options ? {
     undef   => $users_client_options,
+    ''      => $users_client_options,
     default => $hiera_users_client_options,
   }
 

manifests/client.pp
@@ -9,6 +9,7 @@ class ssh::client(
 
   $fin_options = $hiera_options ? {
     undef   => $options,
+    ''      => $options,
     default => $hiera_options,
   }
 

commit 4a02feb8f136488d18ff865f02fa7ac9d08fdf1f

Fix copypaste typo
Introduced in 8aa2b7bb987120c6b46238d158064fb83e4790ec.

manifests/init.pp
@@ -22,7 +22,7 @@ class ssh (
 
   $fin_server_options = $hiera_server_options ? {
     undef   => $server_options,
-    ''      => $client_options,
+    ''      => $server_options,
     default => $hiera_server_options,
   }
 