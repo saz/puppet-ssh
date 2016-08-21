2acb7db207ecb6bc2d7bb6f0f49bd5ccbcae6682
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-08-21 08:48:48 +0200

    Fix additional server options in template, add proper options and matchblock ordering

--- manifests/server/options.pp
+++ manifests/server/options.pp
@@ -2,6 +2,6 @@ define ssh::server::options ($options, $order = 50) {
   concat::fragment { "options ${name}":
     target  => $ssh::params::sshd_config,
     content => template("${module_name}/options.erb"),
-    order   => $order,
+    order   => 100+$order,
   }
 }
