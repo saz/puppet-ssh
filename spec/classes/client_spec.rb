b99e816b34300c5791eea77bbdad7b6c07f74b22
Steffen Zieger <me@saz.sh>
2016-08-26 10:33:08 +0200

    fix ipaddress facts

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -7,6 +7,7 @@ describe 'ssh::client', type: 'class' do
         osfamily: 'Debian',
         interfaces: 'eth0',
         ipaddress_eth0: '192.168.1.1',
+        ipaddress6_eth0: '::1',
         concat_basedir: '/tmp',
         puppetversion: '3.7.0'
       }
@@ -21,6 +22,7 @@ describe 'ssh::client', type: 'class' do
         osfamily: 'Debian',
         interfaces: 'eth0',
         ipaddress_eth0: '192.168.1.1',
+        ipaddress6_eth0: '::1',
         concat_basedir: '/tmp',
         puppetversion: '3.7.0'
       }
