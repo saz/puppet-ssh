0cc1b1f1a14668f4a08d17a6887cb405b610161d
Steffen Zieger <me@saz.sh>
2018-12-14 15:44:43 +0100

    use ensure_packages in server/client install.pp

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,10 +1,6 @@
 class ssh::server::install {
   include ::ssh::params
   if $ssh::params::server_package_name {
-    if !defined(Package[$ssh::params::server_package_name]) {
-      package { $ssh::params::server_package_name:
-        ensure   => $ssh::server::ensure,
-      }
-    }
+    ensure_packages([$ssh::params::server_package_name], {'ensure' => $ssh::server::ensure})
   }
 }
