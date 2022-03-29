11ebe8a1a19f6f196956fadd1415534c27178ed4
Takashi Kajinami <tkajinam@redhat.com>
2022-03-29 20:23:20 +0900

    Ensure valid reference to ssh::params
    This change ensures that ssh::params is included before its variable
    is referenced.

--- manifests/server/match_block.pp
+++ manifests/server/match_block.pp
@@ -7,7 +7,7 @@ define ssh::server::match_block (
   Hash $options  = {},
   String $type   = 'user',
   Integer $order = 50,
-  Stdlib::Absolutepath $target = $ssh::params::sshd_config,
+  Optional[Stdlib::Absolutepath] $target = undef,
 ) {
 
   include ssh::server
@@ -15,7 +15,7 @@ define ssh::server::match_block (
     fail('ssh::server::match_block() define not supported with use_augeas = true')
   } else {
     concat::fragment { "match_block ${name}":
-      target  => $target,
+      target  => pick($target, $ssh::params::sshd_config),
       content => template("${module_name}/sshd_match_block.erb"),
       order   => 200+$order,
     }
