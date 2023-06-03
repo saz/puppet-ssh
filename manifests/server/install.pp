03b28c1464e5d35bb3be42cdec40d1bda2412508
Tim Meusel <tim@bastelfreak.de>
2023-06-03 20:12:37 +0200

    puppetlabs/stdlib: Require 9.x

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -7,7 +7,7 @@ class ssh::server::install {
   assert_private()
 
   if $ssh::server::server_package_name {
-    ensure_packages ([
+    stdlib::ensure_packages ([
         $ssh::server::server_package_name,
       ], {
         'ensure' => $ssh::server::ensure,
